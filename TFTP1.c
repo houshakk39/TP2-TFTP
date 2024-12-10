2390b6f2d2502683bf982ed65337731f889ff253
HAKKI Houssam <hakki.houssam.24@gmail.com>
2024-12-10 10:58:47 +0100

    Update TFTP1.c

--- TFTP1.c
+++ TFTP1.c
@@ -1,23 +1,39 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
+#include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        fprintf(stderr, "Usage: %s <host> <file>\n", argv[0]);
-        exit(EXIT_FAILURE);
+    if (argc != 4) {
+        printf("Usage: %s <host> <file> <port>\n", argv[0]);
+        return 1;
     }
+
     int e;
     struct addrinfo hints;
-    struct addrinfo *res;
+    struct addrinfo *res, *r;
 
-    // Effacer la structure hints et définir les critères
+    // Configuration des attributs de la structure hints
     memset(&hints, 0, sizeof(struct addrinfo));
-    hints.ai_family = AF_INET;       // Utilisation d'IPv4
-    hints.ai_socktype = SOCK_DGRAM; // Protocole UDP
-    hints.ai_protocol = IPPROTO_UDP;
+    hints.ai_family = AF_INET;       // IPv4 uniquement
+    hints.ai_socktype = SOCK_DGRAM;  // Socket UDP
+    hints.ai_protocol = IPPROTO_UDP;  // Protocole UDP
 
+    // Résolution de l'adresse du serveur
+    e = getaddrinfo(argv[1], "69", &hints, &res);
+    if (e != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(e));
+        exit(EXIT_FAILURE);
+    }
+
+    // Parcourir et afficher chaque adresse résolue
+    for (r = res; r != NULL; r = r->ai_next) {
+        printf("Famille = %d, Type Socket = %d, Protocole = %d\n", 
+               r->ai_family, r->ai_socktype, r->ai_protocol);
+    }
+
+    freeaddrinfo(res); 
+    exit(EXIT_SUCCESS);
 }
