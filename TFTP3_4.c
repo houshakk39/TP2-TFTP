63f3e411dcfdb772b4ad339ee22a6b1856758b32
HAKKI Houssam <hakki.houssam.24@gmail.com>
2024-12-17 09:00:03 +0100

    Update TFTP3_4.c

--- TFTP3_4.c
+++ TFTP3_4.c
@@ -10,9 +10,9 @@ int main(int argc, char *argv[]) {
         printf("Usage: %s <host> <file> <port>\n", argv[0]);
         return 1;
     }
-// Création du socket
-int sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-if (sockfd < 0) {
+    // Creation of UDP socket
+    int sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (sockfd < 0) {
     perror("Erreur lors de la création du socket");
     exit(EXIT_FAILURE);
 }
@@ -21,40 +21,43 @@ if (sockfd < 0) {
     struct addrinfo hints;
     struct addrinfo *res, *r;
 
-    // Configuration des attributs de la structure hints
+    // Configuration of attributes of hint structures
     memset(&hints, 0, sizeof(struct addrinfo));
     hints.ai_family = AF_INET;       // IPv4 uniquement
     hints.ai_socktype = SOCK_DGRAM;  // Socket UDP
     hints.ai_protocol = IPPROTO_UDP;  // Protocole UDP
 
-    // Résolution de l'adresse du serveur
+    // Resolution of the server's address
     e = getaddrinfo(argv[1], argv[3], &hints, &res);
     if (e != 0) {
         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(e));
         exit(EXIT_FAILURE);
     }
 
-    // Configuration de l'adresse du serveur
+    // Configuration of server's address
     struct sockaddr_in server_addr;
     memcpy(&server_addr, res->ai_addr, res->ai_addrlen);
     freeaddrinfo(res);
 
-    // Préparation du message à envoyer
+    // preparing the message to send
     char bufT[300] = "Message depuis le client UDP !";
     int msg_len = strlen(bufT);
 
-    // Envoi du message via sendto()
+    // send message via sendto()
     if (sendto(sockfd, bufT, msg_len, 0, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
         perror("Erreur lors de l'envoi du message");
         exit(EXIT_FAILURE);
     }
-
-    // Parcourir et afficher chaque adresse résolue
+	printf("Message envoyé au serveur %s sur le port %s.\n", argv[1], argv[3]);
+	
+    // browse and print every resolved address
     for (r = res; r != NULL; r = r->ai_next) {
         printf("Famille = %d, Type Socket = %d, Protocole = %d\n", 
                r->ai_family, r->ai_socktype, r->ai_protocol);
     }
-
+    // freeing resources
+	close(sockfd);
     freeaddrinfo(res); 
     exit(EXIT_SUCCESS);
+    return 0;
 }
